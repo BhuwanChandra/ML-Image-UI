73b52595e5c22d5c0ebc1198b0b7d61ed31b9bb0
BhuwanChandra <bhuwan7779@gmail.com>
2021-10-31 14:29:13 +0530

    fixed responsive issues

--- src/pages/ModelPage.js
+++ src/pages/ModelPage.js
@@ -5,18 +5,22 @@ import FileUpload from "../components/FileUpload";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 
+import Image0 from "../images/image0.jpg";
 import Image1 from "../images/image1.jpg";
 import Image2 from "../images/image2.jpg";
 import Image3 from "../images/image3.jpg";
+import Image4 from "../images/image4.jpg";
+import Image5 from "../images/image5.jpg";
+import Image6 from "../images/image6.jpg";
 
 const responsive = {
   0: { items: 1 },
-  568: { items: 2 },
-  786: { items: 3 },
-  1024: { items: 4 },
+  576: { items: 2 },
+  768: { items: 3 },
+  1200: { items: 4 },
 };
 
-const images = [Image1, Image2, Image3, Image1, Image2, Image3, Image1];
+const images = [Image0, Image1, Image2, Image3, Image4, Image5, Image6];
 
 function ModelPage() {
   const { type } = useParams();
